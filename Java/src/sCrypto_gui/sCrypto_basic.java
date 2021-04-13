6a1c30c05cc82c9f9fd736ac539435070b1edd4f
Deni Klen <dannyklen7@gmail.com>
2021-04-13 21:01:05 +0200

    TA-indicator selection through GUI

--- Java/src/sCrypto_gui/sCrypto_basic.java
+++ Java/src/sCrypto_gui/sCrypto_basic.java
@@ -17,6 +17,7 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.DefaultComboBoxModel;
 
 public class sCrypto_basic {
 
@@ -52,7 +53,7 @@ public class sCrypto_basic {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 350);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -62,18 +63,18 @@ public class sCrypto_basic {
 		
 		txtValue = new JTextField();
 		txtValue.setText("value");
-		txtValue.setBounds(260, 90, 114, 19);
+		txtValue.setBounds(260, 90, 165, 19);
 		frame.getContentPane().add(txtValue);
 		txtValue.setColumns(10);
 		
 		txtQuantity = new JTextField();
 		txtQuantity.setText("quantity");
-		txtQuantity.setBounds(260, 135, 114, 19);
+		txtQuantity.setBounds(260, 135, 165, 19);
 		frame.getContentPane().add(txtQuantity);
 		txtQuantity.setColumns(10);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(260, 39, 114, 24);
+		comboBox.setBounds(260, 39, 165, 24);
 		frame.getContentPane().add(comboBox);
 		comboBox.addItem("BTC");
 		comboBox.addItem("ETH");
@@ -93,7 +94,7 @@ public class sCrypto_basic {
 				System.out.println(quantity);
 			}
 		});
-		btnBuy.setBounds(55, 180, 117, 25);
+		btnBuy.setBounds(55, 180, 165, 25);
 		frame.getContentPane().add(btnBuy);
 		
 		JButton btnSell = new JButton("SELL");
@@ -108,19 +109,28 @@ public class sCrypto_basic {
 				System.out.println(quantity);
 			}
 		});
-		btnSell.setBounds(260, 180, 117, 25);
+		btnSell.setBounds(260, 180, 165, 25);
 		frame.getContentPane().add(btnSell);
 		
 		JLabel lblCrypto = new JLabel("Crypto: ");
-		lblCrypto.setBounds(55, 44, 70, 15);
+		lblCrypto.setBounds(55, 44, 165, 15);
 		frame.getContentPane().add(lblCrypto);
 		
 		JLabel lblPrice = new JLabel("Price: ");
-		lblPrice.setBounds(55, 92, 70, 15);
+		lblPrice.setBounds(55, 92, 165, 15);
 		frame.getContentPane().add(lblPrice);
 		
-		JLabel lblQuantity = new JLabel("Quantity");
-		lblQuantity.setBounds(55, 137, 70, 15);
+		JLabel lblQuantity = new JLabel("Quantity: ");
+		lblQuantity.setBounds(55, 137, 165, 15);
 		frame.getContentPane().add(lblQuantity);
+		
+		JLabel lblMethod = new JLabel("Method: ");
+		lblMethod.setBounds(55, 237, 165, 15);
+		frame.getContentPane().add(lblMethod);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"golden/death cross", "RSI/MACD", "MFI"}));
+		comboBox_1.setBounds(260, 232, 165, 24);
+		frame.getContentPane().add(comboBox_1);
 	}
 }
