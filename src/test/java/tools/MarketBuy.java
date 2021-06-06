b93ab7f366048c2a904cc6ef1e4d29c74df097db
Deni Klen <dannyklen7@gmail.com>
2021-06-06 12:33:12 +0200

    Changed fixed values in buy and sell

--- src/test/java/tools/MarketBuy.java
+++ src/test/java/tools/MarketBuy.java
@@ -1,32 +1,29 @@
 package tools;
-
 import GUI.layouts.HomeLayout;
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.domain.market.TickerPrice;
 import static com.binance.api.client.domain.account.NewOrder.marketBuy;
-import static com.binance.api.client.domain.account.NewOrder.marketSell;
-//import strategy.MainTmp;
 
 public class MarketBuy {
-//    static String symbolMT = MainTmp.tradingPair;
-static String symbolMT = HomeLayout.tradingPair;
-    static String symbol = symbolMT.replace("/","");
+    static String tradingPair= HomeLayout.tradingPair;
     public static String quantityString;
-    public static String pricesCrypto(BinanceApiRestClient client, String symbol){
-        TickerPrice priceString = client.getPrice(symbol);
+    public static String pricesCrypto(BinanceApiRestClient client, String tradingPair){
+        TickerPrice priceString = client.getPrice(tradingPair);
         float priceFloat = Float.parseFloat(priceString.getPrice());
-        float quantityFloat = 10 / priceFloat;
+        float quantityFloat = 12 / priceFloat;
         quantityString = String.format("%.6f", quantityFloat);
         return quantityString;
     }
-    public static void buysCrypto(BinanceApiRestClient client, String symbol, String quantity){
-        client.newOrder(marketBuy(symbol,  quantity));
+    public static void buysCrypto(BinanceApiRestClient client, String tradingPair, String quantity){
+        client.newOrder(marketBuy(tradingPair,  quantity));
     }
-    public static void main(String[] args) {
-        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance("api_key", "secret_key");
+    public static void main() {
+        //System.out.println("kupujem");
+        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance("api", "secret");
         BinanceApiRestClient client = factory.newRestClient();
-        String quantity = pricesCrypto(client, symbol);
-        buysCrypto(client, symbol, quantity);
+        String quantity = pricesCrypto(client, tradingPair);
+        //System.out.println(quantity);
+        buysCrypto(client, tradingPair, quantity);
     }
 }
